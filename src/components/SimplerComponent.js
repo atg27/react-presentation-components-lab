31ec79a497c358603cbe7fc71c8cf8fdb71bdbf7
Andrew <atg27@uw.edu>
2021-04-25 15:49:41 -0700

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,16 @@
-// Code SimplerComponent Here
+import React from 'react';
+import index from '../index'
+
+export const SimplerComponent = (props) => {
+    constructor(props)
+    super(props)
+
+
+    return (
+        <div onClick={this.props.handleClick} >
+            I am just happy
+        </div>
+    
+    )
+}
+
